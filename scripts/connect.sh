dfc4f34e6fb2f166a90b4c2be9546d50146fc2d4
Elton MATHIAS <elton.mathias@sap.com>
2026-01-21 21:08:17 +0100

    init

---
+++ scripts/connect.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# RunPod Connect Script
+# Before running this script, ensure you have already set up your SSH keys in Runpod dashboard.
+
+shift 1
+export TERM=xterm-256color; ssh -i ~/.ssh/runpod/runpod $@
+
