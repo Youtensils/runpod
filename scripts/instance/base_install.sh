e999f74a7a4b06a2687b5b82419880a61c61fe06
Elton MATHIAS <elton.mathias@sap.com>
2026-01-22 07:21:50 +0100

    more scripts

--- scripts/instance/base_install.sh
+++ scripts/instance/base_install.sh
@@ -2,9 +2,14 @@
 # bash <(wget -qO- https://raw.githubusercontent.com/Youtensils/runpod/refs/heads/main/scripts/instance/base_install.sh)
 
 
-BASEDIR=/workspace/ComfyUI
+BASEDIR=/workspace/runpod-slim/ComfyUI/
 
 #install aria2 for faster downloads
 apt-get update 
 apt-get install -y aria2 
 apt-get install -y screen 
+
+mkdir =p mkdir -p $BASEDIR/YT/
+cd $BASEDIR/YT/
+git clone https://github.com/Youtensils/runpod.git
+
